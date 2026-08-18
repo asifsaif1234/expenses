73e40d1dd4b03c575d4a2be51ef09febf217e893
asif saif <asifsaif786@gmail.com>
2026-08-18 16:08:03 +0530

    Adding UI for signuo and signin

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,17 @@
+// app/javascript/controllers/flash_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["message"]
+  
   connect() {
-    this.element.textContent = "Hello World!"
+    // Auto-dismiss after 5 seconds
+    setTimeout(() => {
+      this.dismiss()
+    }, 5000)
+  }
+  
+  dismiss() {
+    this.messageTarget.remove()
   }
 }
